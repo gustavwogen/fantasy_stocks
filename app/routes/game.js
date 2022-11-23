165835dea0645896c96f2d5b47d8ada536c31062
gustavwogen <52404883+gustavwogen@users.noreply.github.com>
2022-11-22 22:54:40 -0500

    restructure views

--- app/routes/game.js
+++ app/routes/game.js
@@ -28,7 +28,7 @@ router.get("/", (req, res) => {
 })
 
 router.get('/create', function (req, res) {
-    res.render('create_games');
+    res.render('game/create_game');
 });
 
 router.post("/create", asyncHandler(async (req, res) => {
@@ -74,7 +74,7 @@ router.get("/:gameId", asyncHandler(async (req, res) => {
     //console.log(holdings);
     //console.log(portfolios);
 
-    res.render('game', {
+    res.render('game/view_game', {
         cash: cashList,
         portfolios: portfolios,
         holdings: holdings,
